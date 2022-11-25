d8effa3a04017fc8e04650e8bcd3270a0e9b5e40
Gustaxz <guxtavogusteible@gmail.com>
2022-11-25 19:51:11 -0300

    ☀ começando layout da página de posts das matérias

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // green pallete
+        'green-pallete-100': "#dad7cd",
+        'green-pallete-200': "#a3b18a",
+        'green-pallete-300': "#588157",
+        'green-pallete-400': "#3a5a40",
+        'green-pallete-500': "#344e41",
+        // blue + green pallete
+        'blue-pallete-100': "#f0f3bd",
+        'blue-pallete-200': "#02c39a",
+        'blue-pallete-300': "#00a896",
+        'blue-pallete-400': "#028090",
+        'blue-pallete-500': "#05668d"
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
