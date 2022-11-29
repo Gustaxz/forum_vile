5b27fde3d571435b7dbde7c5347c12a647b3f82f
Gustaxz <guxtavogusteible@gmail.com>
2022-11-28 22:58:48 -0300

    ✅ Criação do visual completo do menu de post

---
+++ components/editor/TipTap.tsx
@@ -0,0 +1,157 @@
+import { useEditor, EditorContent } from "@tiptap/react"
+
+import StarterKit from "@tiptap/starter-kit"
+import Heading from "@tiptap/extension-heading"
+import OrderedList from "@tiptap/extension-ordered-list"
+import Paragraph from "@tiptap/extension-paragraph"
+import TextAlign from "@tiptap/extension-text-align"
+import { Color } from "@tiptap/extension-color"
+import TextStyle from "@tiptap/extension-text-style"
+
+import { BsTypeBold, BsTypeItalic, BsTypeStrikethrough, BsCode, BsListOl } from "react-icons/bs"
+import { BiHeading, BiAlignJustify, BiAlignLeft, BiAlignRight, BiAlignMiddle } from "react-icons/bi"
+
+const Tiptap = () => {
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			Heading.configure({
+				levels: [1],
+				HTMLAttributes: {
+					class: "text-2xl font-bold",
+				},
+			}),
+			OrderedList.configure({
+				HTMLAttributes: {
+					class: "list-decimal list-inside",
+				},
+			}),
+			Paragraph.configure({
+				HTMLAttributes: {
+					class: "",
+				},
+			}),
+			TextAlign.configure({
+				types: ["heading", "paragraph"],
+			}),
+			TextStyle,
+			Color,
+		],
+		content: "<p>Coloque aqui o corpo do post 🚀</p>",
+		editorProps: {
+			attributes: {
+				class: "bg-white rounded-lg p-2 max-h-32 overflow-y-scroll outline-none scrollbar-thin scrollbar-thumb-blue-pallete-200 scrollbar-thumb-rounded-lg",
+			},
+		},
+	})
+
+	return (
+		<>
+			{editor ? (
+				<div className="flex justify-center items-center gap-3">
+					<BsTypeBold
+						size={32}
+						onClick={() => editor.chain().focus().toggleBold().run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("bold")
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BsTypeItalic
+						size={32}
+						onClick={() => editor.chain().focus().toggleItalic().run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("italic")
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BsTypeStrikethrough
+						size={32}
+						onClick={() => editor.chain().focus().toggleStrike().run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("strike")
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BsCode
+						size={32}
+						onClick={() => editor.chain().focus().toggleCode().run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("code")
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BiHeading
+						size={32}
+						onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("heading", { level: 1 })
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BsListOl
+						size={32}
+						onClick={() => editor.chain().focus().toggleOrderedList().run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive("orderedList")
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BiAlignLeft
+						size={32}
+						onClick={() => editor.chain().focus().setTextAlign("left").run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive({ textAlign: "left" })
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BiAlignMiddle
+						size={32}
+						onClick={() => editor.chain().focus().setTextAlign("center").run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive({ textAlign: "center" })
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BiAlignRight
+						size={32}
+						onClick={() => editor.chain().focus().setTextAlign("right").run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive({ textAlign: "right" })
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<BiAlignJustify
+						size={32}
+						onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+						className={`cursor-pointer p-1 bg-white border border-blue-pallete-400 rounded-lg hover:opacity-90 ${
+							editor.isActive({ textAlign: "justify" })
+								? "text-blue-pallete-300 font-bold shadow-md shadow-slate-400 border-2"
+								: "text-black"
+						}`}
+					/>
+					<input
+						type="color"
+						onInput={(event: any) =>
+							editor.chain().focus().setColor(event.target.value).run()
+						}
+						className="p-1 rounded-lg border border-blue-pallete-400 cursor-pointer hover:opacity-90"
+						value={editor.getAttributes("textStyle").color}
+					/>
+				</div>
+			) : null}
+			<EditorContent editor={editor} />
+		</>
+	)
+}
+
+export { Tiptap }
--- components/postsPage/CreatePostModal.tsx
+++ components/postsPage/CreatePostModal.tsx
@@ -9,8 +9,23 @@ import {
 
 import { AiOutlinePlus } from "react-icons/ai"
 
+import { Tiptap } from "../editor/TipTap"
+
+import { useState } from "react"
+
+import Multiselect from "multiselect-react-dropdown"
+
+const tagsSelectItems = [
+	{ label: "Atividades", value: "atividades" },
+	{ label: "Dúvidas", value: "duvidas" },
+	{ label: "Ideias", value: "ideias" },
+	{ label: "Problemas", value: "problemas" },
+]
+
 const CreatePostModal = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
+	const [selectedImages, setSelectedImages] = useState<FileList>()
+	const [selectedOtherFiles, setSelectedOtherFiles] = useState<FileList>()
 
 	return (
 		<>
@@ -27,48 +42,86 @@ const CreatePostModal = () => {
 					<ModalCloseButton />
 					<ModalBody className="bg-[url('/backgrounds/create-post.svg')] bg-cover bg-no-repeat">
 						<p className="text-white text-4xl font-semibold">Crie seu post</p>
-						<div className="mt-4 flex flex-col gap-3">
-							<div className="flex flex-col gap-2">
-								<label htmlFor="input-title" className="font-semibold">
-									Título
-								</label>
-								<input
-									type="text"
-									id="input-title"
-									className="w-[75%] rounded-lg p-2 outline-none border-2 border-blue-pallete-200"
-								/>
+						<form onSubmit={(e) => e.preventDefault()}>
+							<div className="mt-4 flex flex-col gap-3">
+								<div className="flex flex-col gap-2">
+									<label htmlFor="input-title" className="font-semibold">
+										Título
+									</label>
+									<input
+										type="text"
+										id="input-title"
+										className="w-[75%] rounded-lg p-2 outline-none border-2 border-blue-pallete-200"
+									/>
+								</div>
+								<Tiptap />
+								<div className="flex gap-3 items-center">
+									<p className="font-semibold mb-1">Campo de imagens:</p>
+									<label
+										htmlFor="input-image"
+										className="h-11 w-11 rounded-lg bg-white border-2 border-blue-pallete-200 flex items-center justify-center cursor-pointer"
+									>
+										<AiOutlinePlus
+											size={24}
+											className="text-blue-pallete-200"
+										/>
+									</label>
+									<input
+										multiple
+										type="file"
+										id="input-image"
+										className="hidden"
+										accept="image/*"
+										onInput={(e: any) => setSelectedImages(e.target.files)}
+									/>
+									<div className="flex gap-2 max-w-[70%]">
+										{ selectedImages ? (
+											Array.from(selectedImages).map(e => <p title={e.name} key={e.size} className="text-ellipsis overflow-hidden whitespace-nowrap">{ e.name }</p>)
+										): null }
+									</div>
+								</div>
+								<div className="flex gap-3 items-center">
+									<p className="font-semibold mb-1">
+										Campo de arquivos (pdf, txt):
+									</p>
+									<label
+										htmlFor="input-files"
+										className="h-11 w-11 rounded-lg bg-white border-2 border-blue-pallete-200 flex items-center justify-center cursor-pointer"
+									>
+										<AiOutlinePlus
+											size={24}
+											className="text-blue-pallete-200"
+										/>
+									</label>
+									<input
+										multiple
+										type="file"
+										id="input-files"
+										className="hidden"
+										accept="application/pdf, .txt"
+										onChange={(e: any) => setSelectedOtherFiles(e.target.files)}
+									/>
+									<div className="flex gap-2 max-w-[70%]">
+										{ selectedOtherFiles ? (
+											Array.from(selectedOtherFiles).map(e => <p title={e.name} key={e.size} className="text-ellipsis overflow-hidden whitespace-nowrap">{ e.name }</p>)
+										): null }
+									</div>
+								</div>
+								<div className="w-[30%]">
+									<Multiselect
+										options={tagsSelectItems}
+										displayValue="label"
+										placeholder="Escolha uma tag"
+										emptyRecordMsg="Nenhuma tag encontrada"
+										className="bg-white rounded-lg cursor-pointer border-2 border-blue-pallete-200"
+										style={{ chips: { background: "#02c39a" } }}
+									/>
+								</div>
+								<button className="absolute bottom-4 w-[18%] hover:opacity-90 py-2 font-semibold rounded-lg bg-white border-2 border-blue-pallete-200 text-blue-pallete-200 flex items-center justify-center cursor-pointe">
+									Enviar
+								</button>
 							</div>
-							<div className="flex gap-3 items-center">
-								<p className="font-semibold mb-1">Campo de imagens:</p>
-								<label
-									htmlFor="input-image"
-									className="h-11 w-11 rounded-lg bg-white border-2 border-blue-pallete-200 flex items-center justify-center cursor-pointer"
-								>
-									<AiOutlinePlus size={24} className="text-blue-pallete-200" />
-								</label>
-								<input
-									type="file"
-									id="input-image"
-									className="hidden"
-									accept="image/*"
-								/>
-							</div>
-							<div className="flex gap-3 items-center">
-								<p className="font-semibold mb-1">Campo de arquivos (pdf, txt):</p>
-								<label
-									htmlFor="input-files"
-									className="h-11 w-11 rounded-lg bg-white border-2 border-blue-pallete-200 flex items-center justify-center cursor-pointer"
-								>
-									<AiOutlinePlus size={24} className="text-blue-pallete-200" />
-								</label>
-								<input
-									type="file"
-									id="input-files"
-									className="hidden"
-									accept="application/pdf, .txt"
-								/>
-							</div>
-						</div>
+						</form>
 					</ModalBody>
 				</ModalContent>
 			</Modal>
